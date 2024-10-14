4ac581541097ab420b9d5bec2ae43fb132edc93d
Huseyin Elibol <elibolh0@gmail.com>
2024-10-14 13:00:19 +0300

    reposiotry layer removed

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,16 +1,16 @@
 import bcrypt from 'bcrypt';
-import UserRepositroy from '../repositories/user.repository';
-import { IUser } from '../models/user.model';
+import UserModel, {IUser} from '../models/user.model';
 
 class UserService {
     public async register(username: string, password: string, email: string): Promise<IUser> {
       const hashedPassword = await bcrypt.hash(password, 10);
       const newUser: IUser = { username, password: hashedPassword, email } as IUser;
-      return await UserRepositroy.createUser(newUser);
+      const user = new UserModel(newUser);
+      return await user.save();
     }
   
     public async validateUser(username: string, password: string): Promise<boolean> {
-        const user = await UserRepositroy.findByUsername(username);
+        const user = await UserModel.findOne({ username });
         if(!user) return false;
         return await bcrypt.compare(password, user.password);
     }
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,21 +1,21 @@
-import TaskRepository from "../repositories/task.repository";
-import {ITask} from '../models/task.model';
+import TaskModel, {ITask} from '../models/task.model';
+
 
 class TaskService {
         public async createTask(taskData: ITask): Promise<ITask>{
-            return await TaskRepository.createTask(taskData);
+            const task = new TaskModel(taskData);
+            return await task.save();
         }
 
         public async getTaskByUserId(userId: string): Promise<ITask[]>{
-            return await TaskRepository.getTasksByUserId(userId);
+            return await TaskModel.find({user: userId});
         }
 
         public async updateTask(taskId: string, updateData: Partial<ITask>):Promise<ITask|null>{
-            return await TaskRepository.updateTask(taskId, updateData);
-        }
+            return await TaskModel.findByIdAndUpdate(taskId, updateData, { new: true });        }
 
         public async deleteTask(taskId: string): Promise<ITask | null> {
-            return await TaskRepository.deleteTask(taskId);
+            return await TaskModel.findByIdAndDelete(taskId);
         }
         
 }
